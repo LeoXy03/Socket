aefa0281fd71899ee7c55f6688826c8904abfbc8
LeoXy03 <lbrenci91@gmail.com>
2022-04-08 15:10:58 +0200

    comunicazione tra client e server

---
+++ src/server.java
@@ -0,0 +1,60 @@
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lbren
+ */
+public class server {
+     
+public class Server {
+public static void main(String[] args) {
+    try {
+    DataOutputStream out;
+    BufferedReader in;
+    ServerSocket serverSocket=new ServerSocket(2000);
+    System.out.println("server avviato correttamente");
+    Socket socket = serverSocket.accept();
+    
+    serverSocket.close();
+    socket.getOutputStream();
+    
+    
+    String messaggio="ciao";
+    out.writeBytes(messaggio+"\r\n");
+    System.out.println("messaggio iniziale");
+    out.flush();
+    
+    String stringaRicevuta;
+    stringaRicevuta=in.readLine();
+    System.out.println("richiesta: " +stringaRicevuta+"\n");
+    //data                  
+    SimpleDateFormat date = new SimpleDateFormat("yyyy.MM.dd.HH:mm:ss");
+    String timeStamp = date.format(new Date());
+                   
+    out.writeBytes(timeStamp+"\r\n");
+    System.out.println("data e ora\n");
+    out.flush();
+              
+    socket.close();
+    System.out.println("fine connessione \n");
+    
+} catch(BindException ex){
+    System.err.println("porta occupata");
+}   catch (IOException ex) {    
+        Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+    }    
+}
